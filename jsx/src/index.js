5f28b296116ccf548700d390470142bb0cc1851d
arnxs <arnxus@gmail.com>
2019-02-01 21:56:02 -0700

    secion 2.19 - variables within jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+    const buttonText = 'Click Me!';
+
     return (
         <div>
             <label className="label" for="name"> Enter Name: </label>
             <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white' }}>Submit</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {buttonText}
+            </button>
         </div>
     );
 };
