8212a9b27aeed03dd9e702098e38de283c7cdd4c
arnxs <arnxus@gmail.com>
2019-02-01 21:38:59 -0700

    render 'Hi There' onto website using basic React Components

---
+++ jsx/src/index.js
@@ -0,0 +1,14 @@
+// Import the react and reactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+    return <div> Hi there! </div>;
+};
+
+// Take the react component and hwo it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
