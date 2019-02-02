669c047de653fa4bb183292a922b0dcda9653656
arnxs <arnxus@gmail.com>
2019-02-01 21:52:29 -0700

    section 2.16

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -8,7 +8,7 @@ const App = () => {
         <div>
             <label class="label" for="name"> Enter Name: </label>
             <input id="name" type="text"/>
-            <button style="background-color: blue; color: white">Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white' }}>Submit</button>
         </div>
     );
 };
