a1fdc05d207f0c4dee71c69dfed8b4d70bd486f4
arnxs <arnxus@gmail.com>
2019-02-03 23:01:34 -0700

    section 3.35 - pass remaining props to component

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail
+                author="Sam"
+                avatar={faker.image.avatar()}
+                comment="Hi"
+                timeAgo="1hr ago"
+            />
+            <CommentDetail
+                author="Alex"
+                avatar={faker.image.avatar()}
+                comment="Hello World"
+                timeAgo="3hrs ago"
+            />
+            <CommentDetail
+                author="Jane"
+                avatar={faker.image.avatar()}
+                comment="Yolo"
+                timeAgo="5min ago"
+            />
         </div>
     );
 };
